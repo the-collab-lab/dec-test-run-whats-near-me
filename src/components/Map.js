6dd4bda93e6a9c2c5e6c71f0eca98b6177be23a1
EJ Mason <eliasjmason@gmail.com>
2020-12-20 17:28:37 -0800

    Center map on user location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+import { useCurrentLocation } from '../lib/userLocation';
+
 const API_KEY = 'AIzaSyAs_fPF3j1pcQYUU2s0WMI27zhV7oe8kks';
 
-export function Map({ lat, lng }) {
-  const [center, setCenter] = useState({ lat: 59.95, lng: 30.33 });
+export function Map() {
+  const { location, error } = useCurrentLocation();
   const [zoom, setZoom] = useState(11);
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: API_KEY }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      />
+      {error && <p>Location Error: {error}</p>}
+      {location && (
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={location}
+          defaultZoom={zoom}
+        />
+      )}
     </div>
   );
 }
